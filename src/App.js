77aacd14cd9c9b36269cdf51fb8dcd0061fade12
Ian Skelskey <ianskelskey@gmail.com>
2023-11-05 18:27:09 -0500

    Add basic layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 function App() {
   return (
-    <div className="bg-gray-800 text-white min-h-screen">
-      <h1>Ian Skelskey</h1>
+    <div className="bg-gray-800 text-white min-h-screen p-8">
+      <header className="flex justify-between items-center py-4">
+        <div className="flex items-center">
+          <img src="/logo256.png" alt="Ian Skelskey Logo" className="h-16 w-16 mr-4 rounded-full" />
+          <h1 className="text-4xl font-bold">Ian Skelskey</h1>
+        </div>
+        <nav>
+          <a href="https://github.com/IanSkelskey" className="text-lg mr-4">GitHub</a>
+          <a href="https://www.linkedin.com/in/ianskelskey/" className="text-lg">LinkedIn</a>
+        </nav>
+      </header>
+      <main>
+        <section className="my-12">
+          <h2 className="text-3xl font-bold mb-4">About Me</h2>
+          <p>
+            I'm a Software Engineering student at Arizona State University and a Shift Supervisor and Barista Trainer at Starbucks. I'm passionate about video game development and design. My goal is to work for a company that makes products I respect, ideally as a software developer or designer.
+          </p>
+        </section>
+        <section className="my-12">
+          <h2 className="text-3xl font-bold mb-4">Projects</h2>
+          {/* Add project cards or list items here */}
+        </section>
+        {/* Add more sections as needed */}
+      </main>
+      <footer className="text-center py-8">
+        <p>© 2023 Ian Skelskey. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-blue': '#1992d4',
+        'brand-red': '#e3342f',
+      },
+      fontFamily: {
+        sans: ['Helvetica', 'Arial', 'sans-serif'],
+      },
+    },
   },
   variants: {
     extend: {},
