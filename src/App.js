14897ecaa96c2b78fa0e5c90236b66ed969029c1
Ian Skelskey <ianskelskey@gmail.com>
2023-11-05 22:17:02 -0500

    Simplify content pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { motion, AnimatePresence } from 'framer-motion';
 import { useState } from 'react';
 import About from './components/About';
-import Skills from './components/Skills';
 import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Education from './components/Education';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
   // State to track which content to show
   const [contentKey, setContentKey] = useState('about');
+  const navigateTo = (key) => {
+    setContentKey(key);
+  };
 
   // Define your animation variants
   const variants = {
@@ -20,11 +22,16 @@ function App() {
   return (
     <div className="dark:bg-gray-800 bg-white dark:text-white text-black min-h-screen p-8">
       <header className="flex-none">
+        <nav className='flex justify-between'>
+          <button className='bg-brand-blue dark:bg-brand-red py-2 px-4 rounded-md' onClick={() => navigateTo('about')}>About</button>
+          <button className='bg-brand-blue dark:bg-brand-red py-2 px-4 rounded-md' onClick={() => navigateTo('projects')}>Projects</button>
+        </nav>
         <div className="flex items-center">
           <img src="/logo256.png" alt="Ian Skelskey Logo" className="h-24 w-24 mr-4 rounded-full" />
           <h1 className="text-4xl font-bold">Ian Skelskey</h1>
         </div>
       </header>
+
       <main className="flex-grow overflow-auto">
         <AnimatePresence>
           {contentKey === 'about' && (
@@ -50,42 +57,6 @@ function App() {
               <Projects />
             </motion.div>
           )}
-          {contentKey === 'skills' && (
-            <motion.div
-              key="skills"
-              variants={variants}
-              initial="initial"
-              animate="animate"
-              exit="exit"
-              className="my-12"
-            >
-              <Skills />
-            </motion.div>
-          )}
-          {contentKey === 'education' && (
-            <motion.div
-              key="education"
-              variants={variants}
-              initial="initial"
-              animate="animate"
-              exit="exit"
-              className="my-12"
-            >
-              <Education />
-            </motion.div>
-          )}
-          {contentKey === 'contact' && (
-            <motion.div
-              key="contact"
-              variants={variants}
-              initial="initial"
-              animate="animate"
-              exit="exit"
-              className="my-12"
-            >
-              <Contact />
-            </motion.div>
-          )}
         </AnimatePresence>
 
 
@@ -94,12 +65,17 @@ function App() {
       {/* Footer */}
       <footer className="flex-none">
         <p>© 2023 Ian Skelskey. All rights reserved.</p>
+        <h2 className="text-3xl font-bold mb-4">Contact</h2>
+        <p>If you're interested in working together, please reach out to me at <a href="mailto:ianskelskey@gmail.com" className="text-brand-blue dark:text-brand-red">ianskelskey@gmail.com</a>.</p>
+        <a href="https://github.com/IanSkelskey" className="text-lg mr-4 hover:text-brand-blue dark:hover:text-brand-red">
+          <FontAwesomeIcon icon={faGithub} size="lg" />
+        </a>
+        <a href="https://www.linkedin.com/in/ianskelskey/" className="text-lg hover:text-brand-blue dark:hover:text-brand-red">
+          <FontAwesomeIcon icon={faLinkedin} size="lg" />
+        </a>
       </footer>
     </div>
   );
 }
-
-// Utility class for skill badges
-const skillBadge = `inline-block bg-gray-200 dark:bg-gray-700 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 dark:text-gray-200 mr-2 mb-2`;
 
 export default App;
--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,29 @@ export default function About() {
             <p>
                 I'm a Software Engineering student at Arizona State University and a Shift Supervisor and Barista Trainer at Starbucks. I'm passionate about video game development and design. My goal is to work for a company that makes products I respect, ideally as a software developer or designer.
             </p>
+            <section className="my-12">
+                <h2 className="text-3xl font-bold mb-4">Education</h2>
+                <div>
+                    <h3 className="text-2xl font-semibold">B.S. in Software Engineering</h3>
+                    <p className="italic">Arizona State University - 2023</p>
+                    <h3 className='text-2xl font-semibold'>A.S. in Computer Science/Mathematics (Honors)</h3>
+                    <p className="italic">Tunxis Community College - 2019</p>
+                </div>
+            </section>
+
+            <section className="my-12">
+                <h2 className="text-3xl font-bold mb-4">Skills</h2>
+                <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+                    {/* Replace with your actual skills */}
+                    <span className="skill-badge">Java</span>
+                    <span className="skill-badge">React</span>
+                    <span className="skill-badge">Tailwind CSS</span>
+                    <span className="skill-badge">Game Development</span>
+                </div>
+            </section>
+
         </section>
     )
 }
+// Utility class for skill badges
+const skillBadge = `inline-block bg-gray-200 dark:bg-gray-700 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 dark:text-gray-200 mr-2 mb-2`;
