2ed99398feaf86f29c72df76523f1a925ad43050
Ian Skelskey <ianskelskey@gmail.com>
2024-01-04 22:38:54 -0500

    Cleanup before bed

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,6 +1,6 @@
 export default function Content({ children }) {
     return (
-        <div className="flex-grow p-6 flex mx-10 max-w-7xl min-h-full bg-theme-cream">
+        <div className="flex-grow p-6 flex mx-10 max-w-7xl min-h-full">
             { children }
         </div>
     )
--- src/components/SplitContentSlide.js
+++ src/components/SplitContentSlide.js
@@ -1,11 +1,11 @@
 export default function SplitContentSlide() {
     return (
-        <div className="flex items-center">
-            <div className="flex-grow p-2">
-                <p className="accent-text">Hello world!</p>
+        <div className="flex items-center p-10">
+            <div className="flex-grow p-2 max-w-3xl">
+                <p className="font-mono accent-text">Hello world!</p>
                 <h1>I'm Ian Skelskey</h1>
-                <p>I'm Ian Skelskey, a passionate full-stack software engineer specializing in front-end technologies. With expertise in React and Tailwind CSS, my focus is on crafting intuitive and effective software solutions. As a Professional Tutor and SI Leader, I've honed my skills in HTML, CSS, JavaScript, and complex networking concepts. Explore my portfolio to see how I blend technical skill with creativity in the dynamic world of software development.</p>
-                <button type="button" className="p-2 bg-theme-blue rounded-md">View my work</button>
+                <blockquote className="py-10 border-l-8 border-l-theme-brown pl-4">I'm Ian Skelskey, a passionate full-stack software engineer specializing in front-end technologies. With expertise in React and Tailwind CSS, my focus is on crafting intuitive and effective software solutions. As a Professional Tutor and SI Leader, I've honed my skills in HTML, CSS, JavaScript, and complex networking concepts. Explore my portfolio to see how I blend technical skill with creativity in the dynamic world of software development.</blockquote>
+                <button type="button" className="p-2 bg-theme-blue rounded-md text-theme-white">View my work</button>
             </div>
             <div className="flex-grow-0 p-2">
                 <img src="/assets/headshot.png" alt="headshot" className="w-full rounded-3xl" />
