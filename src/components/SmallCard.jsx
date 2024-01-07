4f6f85e64eb5ebdc2bce3ff40426488cd150296e
Ian Skelskey <ianskelskey@gmail.com>
2024-01-07 09:51:47 -0500

    Shrink small card icons

--- src/components/SmallCard.jsx
+++ src/components/SmallCard.jsx
@@ -3,27 +3,27 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function SmallCard({ title, description, icon, accentColor }) {
     let iconClass;
     if (accentColor === "red") {
-        iconClass = "text-red-500 text-3xl";
+        iconClass = "text-red-500 text-2xl";
     } else if (accentColor === "blue") {
-        iconClass = "text-blue-500 text-3xl";
+        iconClass = "text-blue-500 text-2xl";
     } else if (accentColor === "green") {
-        iconClass = "text-green-500 text-3xl";
+        iconClass = "text-green-500 text-2xl";
     } else if (accentColor === "yellow") {
-        iconClass = "text-yellow-500 text-3xl";
+        iconClass = "text-yellow-500 text-2xl";
     } else if (accentColor === "purple") {
-        iconClass = "text-purple-500 text-3xl";
+        iconClass = "text-purple-500 text-2xl";
     } else if (accentColor === "pink") {
-        iconClass = "text-pink-500 text-3xl";
+        iconClass = "text-pink-500 text-2xl";
     } else if (accentColor === "indigo") {
-        iconClass = "text-indigo-500 text-3xl";
+        iconClass = "text-indigo-500 text-2xl";
     } else if (accentColor === "gray") {
-        iconClass = "text-gray-500 text-3xl";
+        iconClass = "text-gray-500 text-2xl";
     } else if (accentColor === "black") {
-        iconClass = "text-black text-3xl";
+        iconClass = "text-black text-2xl";
     } else if (accentColor === "white") {
-        iconClass = "text-white text-3xl";
+        iconClass = "text-white text-2xl";
     } else if (accentColor === "rose") {
-        iconClass = "text-rose-500 text-3xl";
+        iconClass = "text-rose-500 text-2xl";
     }
 
 
