10e5d09d6c30dc320c6466a58acae2b423ddad77
Ian Skelskey <ianskelskey@gmail.com>
2024-01-16 21:42:23 -0500

    Remove redundant import

--- src/components/atoms/Icon.js
+++ src/components/atoms/Icon.js
@@ -1,6 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faIcons, siIcons, svgIcons } from "../data/icons";
-import { faIcons, siIcons, svgIcons } from "../data/icons";
 import { createElement } from "react";
 
 const getTextSizeInPx = (className) => {
