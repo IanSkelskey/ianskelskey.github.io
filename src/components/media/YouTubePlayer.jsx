7e95d123e511c517d969610bcb05c5a36e3fb44c
Ian Skelskey <ianskelskey@gmail.com>
2024-02-18 11:44:51 -0500

    Clear warnings and fix icon button links

--- src/components/media/YouTubePlayer.jsx
+++ src/components/media/YouTubePlayer.jsx
@@ -4,9 +4,8 @@ export default function YouTubePlayer({ id, className, title }) {
             className={className}
             src={`https://www.youtube.com/embed/${id}`}
             title={title}
-            frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
+            allowFullScreen
             loading="lazy"
         />
     );
